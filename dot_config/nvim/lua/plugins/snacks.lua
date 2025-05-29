5598dd266a5641e1c265e9b0ad979ffc48648834
Shane Saravia <shane.saravia@snaptravel.com>
2025-05-29 15:46:07 -0400

    show ignored/hidden files

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -1,6 +1,10 @@
 return { -- mostly used for its notifications
   "folke/snacks.nvim",
   opts = {
+    picker = {
+      hidden = true,
+      ignored = true,
+    },
     styles = {
       notification = {
         wo = {
