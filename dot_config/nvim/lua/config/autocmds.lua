217a53a7a5a39c3fd42a7e8dc766e0f79a3f2686
Shane Saravia <shanesaravia@live.ca>
2025-05-31 00:06:55 -0400

    update eslint formatter config

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,14 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Enable EslintFixAll automatically on save via eslint-lsp
+-- local autocmd = vim.api.nvim_create_autocmd
+--
+-- -- Auto format with ESLint on save
+-- autocmd("BufWritePre", {
+--   pattern = { "*.js", "*.jsx", "*.ts", "*.tsx" },
+--   callback = function()
+--     vim.cmd("EslintFixAll")
+--   end,
+-- })
