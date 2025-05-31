acb1f8155e9b5141ddf04238cd7f84178af2af58
Shane Saravia <shane.saravia@snaptravel.com>
2025-05-30 22:15:12 -0400

    add mason config

---
+++ dot_config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "json-lsp",
+        "lua-language-server",
+        "markdown-toc",
+        "markdownlint-cli2",
+        "marksman",
+        "shfmt",
+        "stylua",
+        "taplo",
+        "pyright",
+        "ruff",
+        -- "black",
+        "vtsls",
+        "eslint-lsp",
+        "eslint_d",
+        "prettierd",
+      },
+    },
+  },
+}
